aeeb5b2b1591d8c2e797a9f11ad4b77b269eb44a
clshu <ccss0630@yahoo.com>
2019-09-19 16:36:14 -0700

    Section 4: Void and Never

--- annotation/functions.ts
+++ annotation/functions.ts
@@ -15,5 +15,19 @@ function multiply(a: number, b: number): number {
   return a * b;
 }
 
+const logger = (message: string): void => {
+  console.log(message);
+}
+
+const throwError = (message: string): never => {
+  throw new Error(message);
+}
+
+const throwError1 = (message: string): any => {
+  if (message) {
+    throw new Error(message);
+  }
+}
+
 
 
