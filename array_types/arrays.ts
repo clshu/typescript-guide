39fce5594c189d632c46dbcf1c86d71e42a1034a
clshu <ccss0630@yahoo.com>
2019-09-19 17:51:40 -0700

    Section 5: Why Typed Array

---
+++ array_types/arrays.ts
@@ -0,0 +1,17 @@
+const carMakers = ['ford', 'toyota', 'chevy'];
+const dates = [new Date(), new Date()];
+
+const carsByMake: string[][] = [];
+
+// Help type inference when extracting values from the array
+const car = carMakers[0];
+const myCar = carMakers.pop();
+
+// Prevent imcompatible values
+carMakers.push(100);
+
+// Help with 'map'
+
+carMakers.map((car: string): string => {
+  return car.toUpperCase();
+})
