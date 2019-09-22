a36d6e9d1b5d11dc8e995bdf4428f78af9018a51
clshu <ccss0630@yahoo.com>
2019-09-21 18:32:29 -0700

    Section 10: Refactor code to NumbersCollection class with compare(), swap() and 'get length(). Refactor remaing code as Sorter class'

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,30 +1,7 @@
-class Sorter {
+import { NumbersCollection } from "./NumbersCollection";
+import { Sorter } from './Sorter'
 
-  constructor(public collection: number[] | string) {
-  }
-
-  sort(): void {
-    const { length } = this.collection;
-
-    for (let i = 0; i < length; i++) {
-      for (let j = 0; j < length - 1; j++) {
-        if (this, this.collection instanceof Array) {
-          if (this.collection[j] > this.collection[j + 1]) {
-            const leftHand = this.collection[j]
-            this.collection[j] = this.collection[j + 1]
-            this.collection[j + 1] = leftHand
-          }
-        }
-        if (typeof this.collection === 'string') {
-
-        }
-      }
-    }
-
-  }
-}
-
-const numArr: number[] = [2, 7, 1, 6, 9]
-const sorter = new Sorter(numArr)
+const numbersCollection = new NumbersCollection([2, 7, 1, 6, 9])
+const sorter = new Sorter(numbersCollection)
 sorter.sort()
-console.log(sorter.collection)
+console.log(numbersCollection.data)
