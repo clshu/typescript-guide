46404f8ef0345e82a6c29d73a9a6fe09bac34392
clshu <ccss0630@yahoo.com>
2019-09-21 21:01:07 -0700

    Section 10: Create class CharactersCollection that satisfies interface Sortable

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,7 +1,12 @@
 import { NumbersCollection } from "./NumbersCollection";
 import { Sorter } from './Sorter'
+import { CharactersCollection } from "./CharatersCollection";
 
 const numbersCollection = new NumbersCollection([2, 7, 1, 6, 9])
-const sorter = new Sorter(numbersCollection)
-sorter.sort()
+const sorter1 = new Sorter(numbersCollection)
+sorter1.sort()
 console.log(numbersCollection.data)
+const charactersCollection = new CharactersCollection('ZuluziberaXexA')
+const sorter2 = new Sorter(charactersCollection)
+sorter2.sort()
+console.log(charactersCollection.data)
