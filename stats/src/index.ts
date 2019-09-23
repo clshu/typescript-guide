9abce1a4566905c68ba8ab0db07aa417c4a98cff
clshu <ccss0630@yahoo.com>
2019-09-22 21:12:59 -0700

    Section 11: Use composition instead of inheritance. Refactor CvsFileReader to be a regular class. MatchReader has a property reader which will contains a CvsReaderObject. The loading data job in MatchReader is delegated to CvsFileReader

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,13 +1,16 @@
 import { MatchResults } from './MatchResults'
 import { MatchReader } from './MatchReader'
+import { CsvFileReader } from './CsvFileReader'
 
-const reader = new MatchReader('football.csv')
-reader.read()
-
+// Create an object that satisfies 'DataReader' interface
+const csvFileReader = new CsvFileReader('football.csv')
+// Create an instance of MatchReader and pass a 'DataReader'
+const matchReader = new MatchReader(csvFileReader)
+matchReader.load()
 
 let manUnitedWins = 0
 
-for (let match of reader.data) {
+for (let match of matchReader.matches) {
   if (match[1] === 'Man United' && match[5] === MatchResults.HomeTeamWin) {
     manUnitedWins++
   } else if (match[2] === 'Man United' && match[5] === MatchResults.AwayTeamWin) {
