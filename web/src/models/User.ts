44bbd4dc774a110fdbe87e5db9b8e5f3a30680aa
clshu <ccss0630@yahoo.com>
2019-09-27 19:55:48 -0700

    Section 13: 175 Extracting a Model class

--- web/src/models/User.ts
+++ web/src/models/User.ts
@@ -1,7 +1,4 @@
-import { Sync } from "./Sync"
-import { Eventing } from "./Eventing"
-import { Attributes } from "./Attributes"
-import { AxiosResponse } from "axios"
+import { Model } from "./Model"
 
 const RootUrl = 'http://localhost:3000/users'
 
@@ -11,50 +8,7 @@ export interface UserProps {
   age?: number;
 }
 
-export class User {
-  public events: Eventing = new Eventing()
-  public sync: Sync<UserProps> = new Sync<UserProps>(RootUrl)
-  public attributes: Attributes<UserProps>
+export class User extends Model<UserProps> {
 
-  constructor(private attrs: UserProps) {
-    this.attributes = new Attributes<UserProps>(attrs)
-  }
 
-  get on() {
-    return this.events.on
-  }
-
-  get trigger() {
-    return this.events.trigger
-  }
-
-  get get() {
-    return this.attributes.get
-  }
-
-  set(update: UserProps) {
-    this.attributes.set(update)
-    this.events.trigger('change')
-  }
-
-  fetch(): void {
-    const id = this.get('id')
-    if (typeof id !== 'number') {
-      throw new Error('Cannot fetch without id')
-    }
-    this.sync.fetch(id)
-      .then((response: AxiosResponse): void => {
-        this.set(response.data)
-      })
-  }
-
-  save(): void {
-    this.sync.save(this.attributes.getAll())
-      .then((Response: AxiosResponse): void => {
-        this.trigger('save')
-      })
-      .catch(() => {
-        this.trigger('error')
-      })
-  }
 }
