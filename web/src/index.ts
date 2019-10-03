279914d7ec066f91c312a8eb9a628c8d8806068e
clshu <ccss0630@yahoo.com>
2019-10-03 00:32:46 -0700

    Section 13: 191. Adding Modeling Properties

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,7 +1,10 @@
 import { UserForm } from "./views/UserForm";
+import { User } from "./models/User";
 
+const user = User.buildUser({ name: 'Alan', age: 70 })
 const userForm = new UserForm(
-  document.getElementById('root')
+  document.getElementById('root'),
+  user
 )
 
 userForm.render()
--- web/src/views/UserForm.ts
+++ web/src/views/UserForm.ts
@@ -1,5 +1,7 @@
+import { User } from "../models/User"
+
 export class UserForm {
-  constructor(public parent: Element) { }
+  constructor(public parent: Element, public model: User) { }
 
   eventsMap(): { [key: string]: () => void } {
     return {
@@ -32,6 +34,8 @@ export class UserForm {
     return `
       <div>
         <h1>User Form</h1>
+        <div>Name: ${this.model.get('name')}</div>
+        <div>Age: ${this.model.get('age')}</div>
         <input/>
         <button>Click me</button>
       </div>
