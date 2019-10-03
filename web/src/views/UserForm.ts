1e5118d39cf6e90a3fcc7fe224085f26a9fb57f7
clshu <ccss0630@yahoo.com>
2019-10-03 00:23:14 -0700

    Section 13: 189 - 190. Create an events map. Create event handlers. Bind handler to elements in the fragment

--- web/src/views/UserForm.ts
+++ web/src/views/UserForm.ts
@@ -1,16 +1,48 @@
 export class UserForm {
   constructor(public parent: Element) { }
+
+  eventsMap(): { [key: string]: () => void } {
+    return {
+      'click:button': this.onButtonClick,
+      'mouseenter:h1': this.onH1Hover
+    }
+  }
+
+  onButtonClick(): void {
+    console.log('Hello Click')
+  }
+
+  onH1Hover(): void {
+    console.log('Hovering H1')
+  }
+
+  bindEvents(fragment: DocumentFragment): void {
+    const eventsMap = this.eventsMap()
+
+    for (let eventKey in eventsMap) {
+      const [eventName, selector] = eventKey.split(':')
+
+      fragment.querySelectorAll(selector).forEach(element => {
+        element.addEventListener(eventName, eventsMap[eventKey])
+      });
+    }
+  }
+
   template(): string {
     return `
       <div>
         <h1>User Form</h1>
         <input/>
+        <button>Click me</button>
       </div>
     `
   }
+
   render(): void {
     const templateElement = document.createElement('template')
     templateElement.innerHTML = this.template()
+    this.bindEvents(templateElement.content)
+    // templateElement.content.querySelector('button').addEventListener('click', this.onClickButton)
     this.parent.append(templateElement.content)
   }
 } 
