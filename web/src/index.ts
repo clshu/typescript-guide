2354f732f4ebdaf787affc5032bebb7c5de9e861
clshu <ccss0630@yahoo.com>
2019-10-02 19:01:20 -0700

    Section 13: 183. Generic User Collection

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,6 +1,9 @@
 import { Collection } from './models/Collection'
-import { RootUrl } from "./models/User"
-const collection = new Collection(RootUrl)
+import { RootUrl, User, UserProps } from "./models/User"
+const collection = new Collection<User, UserProps>(
+  RootUrl,
+  (json: UserProps) => User.buildUser(json)
+)
 
 collection.on('change', () => {
   console.log(collection)
