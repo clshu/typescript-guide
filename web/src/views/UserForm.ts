cad6c2951537e1d82f0fb2e6f75d32156b7ce9be
clshu <ccss0630@yahoo.com>
2019-10-03 01:59:10 -0700

    Section 13: 197 - 198. Composition vs Inheritance. Inheritance is better this time. Extracting a abstract View class.

--- web/src/views/UserForm.ts
+++ web/src/views/UserForm.ts
@@ -1,15 +1,8 @@
 import { User } from "../models/User"
+import { View } from "./view"
 
-export class UserForm {
-  constructor(public parent: Element, public model: User) {
-    this.bindModel()
-  }
+export class UserForm extends View {
 
-  bindModel(): void {
-    this.model.on('change', () => {
-      this.render()
-    })
-  }
 
   eventsMap(): { [key: string]: () => void } {
     return {
@@ -30,18 +23,6 @@ export class UserForm {
     }
   }
 
-  bindEvents(fragment: DocumentFragment): void {
-    const eventsMap = this.eventsMap()
-
-    for (let eventKey in eventsMap) {
-      const [eventName, selector] = eventKey.split(':')
-
-      fragment.querySelectorAll(selector).forEach(element => {
-        element.addEventListener(eventName, eventsMap[eventKey])
-      });
-    }
-  }
-
   template(): string {
     return `
       <div>
@@ -53,14 +34,5 @@ export class UserForm {
         <button class="set-age">Set Random Age</button>
       </div>
     `
-  }
-
-  render(): void {
-    this.parent.innerHTML = ''
-    const templateElement = document.createElement('template')
-    templateElement.innerHTML = this.template()
-    this.bindEvents(templateElement.content)
-    // templateElement.content.querySelector('button').addEventListener('click', this.onClickButton)
-    this.parent.append(templateElement.content)
   }
 } 
