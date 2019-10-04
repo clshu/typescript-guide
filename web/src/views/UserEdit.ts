36da021838bd26e0b8c3d6bd10b9a59a0ad0d210
clshu <ccss0630@yahoo.com>
2019-10-04 00:06:42 -0700

    Section 13: 202 - 205. Create an eventsMap() and regions in View to map nest child views. When a View has children, children Views is inserted in .class-name

--- web/src/views/UserEdit.ts
+++ web/src/views/UserEdit.ts
@@ -1,8 +1,22 @@
 import { View } from "./view";
 import { User, UserProps } from "../models/User";
+import { UserShow } from "./UserShow";
+import { UserForm } from "./UserForm";
 
 
-export class UserShow extends View<User, UserProps>{
+export class UserEdit extends View<User, UserProps>{
+  regionsMap(): { [key: string]: string } {
+    return {
+      userShow: '.user-show',
+      userForm: '.user-form'
+    }
+  }
+
+  onRender() {
+    new UserShow(this.regions.userShow, this.model).render()
+    new UserForm(this.regions.userForm, this.model).render()
+  }
+
   template(): string {
     return `
     <div>
